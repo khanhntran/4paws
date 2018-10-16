d612d476b9526619be045115f4f63643cde3ec3d
Acacia Dai <acacia.dai@gmail.com>
2018-10-15 23:37:01 -0400

    Copied in contact info content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,17 +5,20 @@ const ContactPage = () => {
   return (
     <div>
       <Layout>
-        <h1>Contact Page</h1>
+        <h1>Contact Us</h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia,
-          quisquam harum iure quasi dolores incidunt debitis sed impedit placeat
-          repellendus laborum rem amet tempora ea obcaecati temporibus fuga?
-          Mollitia, nemo.
+          <b>4Paws Rescue Team</b><br></br>
+          P.O. Box 2908<br></br>
+          Merrifield, Virginia 22116
         </p>
+        <p>
+        <b>Phone:</b> 703-715-6369<br></br>
+        <b>Fax:</b> 703-560-9795<br></br>
+        <b>Email:</b> info@fourpaws.org
+        </p>
+        <br></br>
         <h1>To Do:</h1>
         <ul>
-          <li>Email</li>
-          <li>Phone number</li>
           <li>Social Media</li>
           <li>Contact form</li>
         </ul>
