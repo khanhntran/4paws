5346bdb33499660e2ab26fe23e0f73078ba9b47b
Acacia Dai <acacia.dai@gmail.com>
2018-10-16 00:53:49 -0400

    Added more resources content pages

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Link from 'gatsby-link';
 
 const ResourcesPage = () => (
   <Layout>
     <h1>Resources Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iste
-      officia cumque ut minus magnam non doloribus fuga illum expedita repellat
-      veniam totam perspiciatis, ex deleniti omnis eos saepe facilis tempore
-      sunt repudiandae distinctio soluta. Distinctio nihil placeat sunt facere!
-    </p>
     <h2>To Do:</h2>
     <h3>Have each one as its own MD</h3>
     <ul>
       <li>Adoption form</li>
-      <li>Information on owning a cat</li>
-      <li>Why to spay/neuter</li>
+      <li><Link to="/resource-pages/ready-to-adopt-info">Information on owning a cat</Link></li>
+      <li><Link to="/resource-pages/spay-neuter-info">Why to spay/neuter</Link></li>
       <li>Why you shouldn't declaw</li>
       <li>Introucing Cats</li>
-      <li>Facts about FIV and FELV</li>
+      <li><Link to="/resource-pages/fiv-felv-info">Facts about FIV and FELV</Link></li>
     </ul>
   </Layout>
 );
