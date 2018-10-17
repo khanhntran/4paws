b46b66ec87da2073207cef8731906616f80cbd47
Khanh Tran <khanhtran@Khanhs-MacBook-Pro.local>
2018-10-16 21:39:12 -0400

    added normalize.css and change link import from gatsby-link to just gatsby

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Link from 'gatsby-link';
+import { Link } from 'gatsby';
 
 const ResourcesPage = () => (
   <Layout>
@@ -9,11 +9,29 @@ const ResourcesPage = () => (
     <h3>Have each one as its own MD</h3>
     <ul>
       <li>Adoption form</li>
-      <li><Link to="/resource-pages/ready-to-adopt-resource">Information on owning a cat</Link></li>
-      <li><Link to="/resource-pages/spay-neuter-resource">Why to spay/neuter</Link></li>
-      <li><Link to="/resource-pages/introducing-cats-resource">Introducing Cats</Link></li>
-      <li><Link to="/resource-pages/fiv-felv-resource">Facts about FIV and FELV</Link></li>
-      <li><Link to="/resource-pages/adoption-locations">Adoption Locations</Link></li>
+      <li>
+        <Link to="/resource-pages/ready-to-adopt-resource">
+          Information on owning a cat
+        </Link>
+      </li>
+      <li>
+        <Link to="/resource-pages/spay-neuter-resource">
+          Why to spay/neuter
+        </Link>
+      </li>
+      <li>
+        <Link to="/resource-pages/introducing-cats-resource">
+          Introducing Cats
+        </Link>
+      </li>
+      <li>
+        <Link to="/resource-pages/fiv-felv-resource">
+          Facts about FIV and FELV
+        </Link>
+      </li>
+      <li>
+        <Link to="/resource-pages/adoption-locations">Adoption Locations</Link>
+      </li>
     </ul>
   </Layout>
 );
