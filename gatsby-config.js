38bb07559d7b8661608a20d7c4566c6dc4c04da5
khanhntran@users.noreply.github.com <khanhtran@Khanhs-MacBook-Pro.local>
2018-10-21 15:51:51 -0400

    added netlify cms and defined cms backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     'gatsby-plugin-catch-links',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify-cms',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
